b4265a3cb75658ee6c1e05ccdc9b4767f65d63b0
Chris Harris <chris.harris@kitware.com>
2018-12-12 19:34:49 -0500

    Add naive python example for preview image

---
+++ examples/stem_image.py
@@ -0,0 +1,29 @@
+import glob
+from stempy import io, image
+import numpy as np
+from PIL import Image
+
+def save_img(stem_image_data, name):
+    min = np.min(stem_image_data)
+    max = np.max(stem_image_data)
+
+    stem_image_data = stem_image_data.reshape((160, 160))
+    stem_image_data = np.interp(stem_image_data, [min, max], [0, 256])
+    stem_image_data = stem_image_data.astype(np.uint8)
+    img = Image.fromarray(stem_image_data)
+    img.save(name)
+
+stem_image_data_day = np.zeros((160*160,), dtype=float)
+stem_image_data_night = np.zeros((160*160,), dtype=float)
+
+mask_size = 20
+blocks = []
+for f in glob.glob('/data/4dstem/smallScanningDiffraction/data*.dat'):
+    reader = io.reader(f)
+    for b in reader:
+        blocks.append(b)
+
+img = image.create_stem_image(blocks, 160, 160,  40, 288);
+
+save_img(img.bright, 'bright.png')
+save_img(img.dark, 'dark.png')
