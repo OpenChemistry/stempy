a068cd343caca1e320f6adc27978a6d963773e82
Patrick Avery <patrick.avery@kitware.com>
2019-04-08 13:58:48 -0400

    Added first draft of using VTKm for parallel sum
    Signed-off-by: Patrick Avery <patrick.avery@kitware.com>

--- stempy/image.cpp
+++ stempy/image.cpp
@@ -1,5 +1,11 @@
 #include "image.h"
+
+#include "config.h"
 #include "mask.h"
+
+#ifdef VTKm
+#include <vtkm/cont/Algorithm.h>
+#endif
 
 #include <memory>
 
@@ -36,6 +42,55 @@ STEMValues calculateSTEMValues(uint16_t data[], int offset,
   return stemValues;
 }
 
+#ifdef VTKm
+STEMValues calculateSTEMValuesParallel(uint16_t data[], int offset,
+                                       int numberOfPixels,
+                                       uint16_t brightFieldMask[],
+                                       uint16_t darkFieldMask[],
+                                       uint32_t imageNumber)
+{
+  STEMValues stemValues;
+  stemValues.imageNumber = imageNumber;
+
+  auto keysBright = vtkm::cont::make_ArrayHandle(brightFieldMask,
+                                                 numberOfPixels);
+  auto keysDark = vtkm::cont::make_ArrayHandle(darkFieldMask, numberOfPixels);
+  auto input = vtkm::cont::make_ArrayHandle(&data[offset], numberOfPixels);
+
+  vtkm::cont::ArrayHandle<uint16_t> uniqueKeys;
+  vtkm::cont::ArrayHandle<uint16_t> sums;
+
+  // Portals so we may view the values
+  auto uniqueKeysPortal = uniqueKeys.GetPortalConstControl();
+  auto sumsPortal = sums.GetPortalConstControl();
+
+  // Bright
+  vtkm::cont::Algorithm::ReduceByKey(keysBright, input, uniqueKeys, sums,
+                                     vtkm::Add());
+
+  // Find the index of 0xFFFF
+  for (vtkm::Id i = 0; i < uniqueKeysPortal.GetNumberOfValues(); ++i) {
+    if (uniqueKeysPortal.Get(i) == static_cast<uint16_t>(0xFFFF)) {
+      stemValues.bright = sumsPortal.Get(i);
+      break;
+    }
+  }
+
+  // Dark
+  vtkm::cont::Algorithm::ReduceByKey(keysDark, input, uniqueKeys, sums,
+                                     vtkm::Add());
+
+  // Find the index of 0xFFFF
+  for (vtkm::Id i = 0; i < uniqueKeysPortal.GetNumberOfValues(); ++i) {
+    if (uniqueKeysPortal.Get(i) == static_cast<uint16_t>(0xFFFF)) {
+      stemValues.dark = sumsPortal.Get(i);
+      break;
+    }
+  }
+
+  return stemValues;
+}
+#endif
 
 STEMImage createSTEMImage(std::vector<Block>& blocks, int rows, int columns,  int innerRadius, int outerRadius)
 {
