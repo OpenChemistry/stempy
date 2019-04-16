0e6039711998bc8a225a607be97c6f8e3647770c
Patrick Avery <patrick.avery@kitware.com>
2019-04-16 13:35:45 -0400

    Remove image saving from benchmark script
    Signed-off-by: Patrick Avery <patrick.avery@kitware.com>

--- examples/benchmark_stem_image.py
+++ examples/benchmark_stem_image.py
@@ -1,18 +1,6 @@
 import glob
 from stempy import io, image
-import numpy as np
-from PIL import Image
 import time
-
-def save_img(stem_image_data, name):
-    min = np.min(stem_image_data)
-    max = np.max(stem_image_data)
-
-    stem_image_data = stem_image_data.reshape((160, 160))
-    stem_image_data = np.interp(stem_image_data, [min, max], [0, 256])
-    stem_image_data = stem_image_data.astype(np.uint8)
-    img = Image.fromarray(stem_image_data)
-    img.save(name)
 
 # Beware of disk caching when using this benchmark
 num_runs = 10
@@ -33,6 +21,3 @@ for i in range(num_runs):
 
 print('Number of runs was:', num_runs)
 print('Average time: {:0.2f} seconds'.format(sum(times) / num_runs))
-
-save_img(img.bright, 'bright.png')
-save_img(img.dark, 'dark.png')
