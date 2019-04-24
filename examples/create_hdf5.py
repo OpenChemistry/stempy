c54eef76687700ad9ce351fcf827a2bed7fe3aa6
Patrick Avery <patrick.avery@kitware.com>
2019-04-24 11:18:22 -0400

    Add click options for create_stem_image arguments
    Signed-off-by: Patrick Avery <patrick.avery@kitware.com>

--- examples/create_hdf5.py
+++ examples/create_hdf5.py
@@ -5,8 +5,12 @@ from stempy import io, image
 
 @click.command()
 @click.argument('files', nargs=-1, type=click.Path(exists=True, dir_okay=False))
+@click.option('-r', '--rows', default=160, help='Number of rows')
+@click.option('-c', '--columns', default=160, help='Number of columns')
+@click.option('-i', '--inner-radius', default=40, help='Mask inner radius')
+@click.option('-u', '--outer-radius', default=288, help='Mask outer radius')
 @click.option('-o', '--output', default='stem_image.h5', help='Output file')
-def make_stem_hdf5(files, output):
+def make_stem_hdf5(files, rows, columns, inner_radius, outer_radius, output):
     """Make an HDF5 file containing a STEM image
 
     Example: "python create_hdf5.py /path/to/data/data*.dat"
@@ -17,7 +21,8 @@ def make_stem_hdf5(files, output):
         sys.exit('No files found')
 
     reader = io.reader(files)
-    img = image.create_stem_image(reader, 160, 160,  40, 288);
+    img = image.create_stem_image(reader, rows, columns, inner_radius,
+                                  outer_radius);
 
     io.save_stem_image(output, img)
 
