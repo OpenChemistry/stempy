57f25e5a83cc170eeb76860f062f5db55f3bd398
Patrick Avery <patrick.avery@kitware.com>
2019-04-25 10:50:16 -0400

    Calculate and save electron count in H5 file also
    Several TODO's still exist, but it works for the sample
    that I tried.
    
    I created a sample H5 file using the command as follows:
    ```
    python create_hdf5.py -d ~/data/stempy/4dstem/electronCounting/stem4d_0000000235_0000000001.dat ~/data/stempy/4dstem/electronCounting/stem4d_0000000236_0000000009.dat -r 40 -c 40
    ```
    
    The sample H5 file has the following structure:
    ```
    electron_events
    ├── frames
    └── scan_positions
    stem
    ├── bright
    └── dark
    ```
    
    Signed-off-by: Patrick Avery <patrick.avery@kitware.com>

--- examples/create_hdf5.py
+++ examples/create_hdf5.py
@@ -9,8 +9,10 @@ from stempy import io, image
 @click.option('-c', '--columns', default=160, help='Number of columns')
 @click.option('-i', '--inner-radius', default=40, help='Mask inner radius')
 @click.option('-u', '--outer-radius', default=288, help='Mask outer radius')
+@click.option('-d', '--dark-sample', help='Dark sample file')
 @click.option('-o', '--output', default='stem_image.h5', help='Output file')
-def make_stem_hdf5(files, rows, columns, inner_radius, outer_radius, output):
+def make_stem_hdf5(files, rows, columns, inner_radius, outer_radius,
+                   dark_sample, output):
     """Make an HDF5 file containing a STEM image
 
     Example: "python create_hdf5.py /path/to/data/data*.dat"
@@ -20,10 +22,25 @@ def make_stem_hdf5(files, rows, columns, inner_radius, outer_radius, output):
     if len(files) == 0:
         sys.exit('No files found')
 
-    reader = io.reader(files)
+    # VERSION2 is currently hardcoded for dark sample, should be a CLI
+    # option in the future.
+    reader = io.reader(dark_sample, version=io.FileVersion.VERSION2)
+    blocks = [b for b in reader]
+    dark = image.calculate_average(blocks)
+
+    reader = io.reader(files, version=io.FileVersion.VERSION2)
+    blocks = [b for b in reader]
+
+    # TODO: Make these CLI options
+    scan_nx = 40
+    scan_ny = 40
+    frame_events = image.electron_count(blocks, scan_nx, scan_ny, dark)
+
+    reader = io.reader(files, version=io.FileVersion.VERSION2)
     img = image.create_stem_image(reader, rows, columns, inner_radius,
                                   outer_radius);
 
+    io.save_electron_counts(output, frame_events, scan_nx, scan_ny)
     io.save_stem_image(output, img)
 
 if __name__ == '__main__':
