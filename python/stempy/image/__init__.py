5a71f8151a08623700f69d926788419f2ebe2975
Patrick Avery <patrick.avery@kitware.com>
2019-05-15 12:34:07 -0400

    Added python function to create sparse stem image
    Added a python function wrap and use the C++ createSTEMImageSparse()
    function.
    
    Signed-off-by: Patrick Avery <patrick.avery@kitware.com>

--- python/stempy/image/__init__.py
+++ python/stempy/image/__init__.py
@@ -13,6 +13,18 @@ def create_stem_image(reader, inner_radius,
 
     return image
 
+def create_stem_image_sparse(data, inner_radius, outer_radius,
+                             rows, columns, frame_rows, frame_columns):
+    img =  _image.create_stem_image_sparse(data, inner_radius, outer_radius,
+                                           rows, columns, frame_rows,
+                                           frame_columns)
+
+    image = namedtuple('STEMImage', ['bright', 'dark'])
+    image.bright = np.array(img.bright, copy = False)
+    image.dark = np.array(img.dark, copy = False)
+
+    return image
+
 class ImageArray(np.ndarray):
     def __new__(cls, array, dtype=None, order=None):
         obj = np.asarray(array, dtype=dtype, order=order).view(cls)
