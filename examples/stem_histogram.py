61868ba2ff225b42ff3ab180521e5dde3114a090
Zhuokai Zhao <zhuokai@uchicago.edu>
2019-06-22 16:35:50 -0400

    stuck on python example

--- examples/stem_histogram.py
+++ examples/stem_histogram.py
@@ -1,14 +1,17 @@
 import glob
 import sys
+import os
 from stempy import io, image
 import numpy as np
 from PIL import Image
+import matplotlib.pyplot as plt
 
 def print_help():
     print(
     '''
     Usage:
     python3 stem_histogram.py [dataDir -- path to the data directory]
+                              [outDir -- path to the output image directory]
     ''')
 
 def save_img(stem_image_data, name):
@@ -25,25 +28,47 @@ def save_img(stem_image_data, name):
 def main(argv):
     print_help()
     dataDir = argv[0] if len(argv) > 0 else '/home/zhuokai/Desktop/data/data*.dat'
+    outDir = argv[1] if len(argv) > 1 else '/home/zhuokai/Desktop/stempy/examples/output/'
 
-    stem_image_data_day = np.zeros((160*160,), dtype=float)
-    stem_image_data_night = np.zeros((160*160,), dtype=float)
+    # create output directory if it does not exit
+    if not os.path.exists(outDir):
+        os.mkdir(outDir)
+        print("Output directory", outDir , "created ")
+    else:    
+        print("Outout directory", outDir , "already exists")
 
-    mask_size = 20
+    # get the all the data files
     files = []
     for f in glob.glob(dataDir):
         files.append(f)
 
-    inner_radii = [0, 40]
-    outer_radii = [288, 288]
+    inner_radii = [40]
+    outer_radii = [288]
 
     reader = io.reader(files)
     imgs = image.create_stem_images(reader, inner_radii, outer_radii, width=160,
                                     height=160)
 
+    # save the STEM images
     for i, img in enumerate(imgs):
         suffix = str(inner_radii[i]) + '_' + str(outer_radii[i]) + '.png'
-        save_img(img, 'img_' + suffix)
+        save_img(img, outDir + 'img_' + suffix)
+
+    print("STEM images have been saved")
+
+    # generate histograms
+    numBins = 100
+    for i, img in enumerate(imgs):
+        print('Generating histogram for STEM image' + str(i))
+        suffix = str(inner_radii[i]) + '_' + str(outer_radii[i]) + '.png'
+        # curHistogram is int numpy array, convert to histogram
+        curHistogram = image.create_stem_histogram(img, numBins)
+        fig = plt.hist(a,normed=0)
+        plt.title('Histogram of STEM image')
+        plt.xlabel("Value")
+        plt.ylabel("Frequency")
+        plt.savefig(outDir + 'histogram_' + suffix)
+
 
 if __name__ == "__main__":
     main(sys.argv[1:])
--- stempy/image.cpp
+++ stempy/image.cpp
@@ -281,6 +281,85 @@ vector<STEMImage> createSTEMImages(InputIt first, InputIt last,
   return images;
 }
 
+namespace {
+// helper function that returns the binding result
+std::vector<float> getContainer(const STEMImage& inImage, const int numBins)
+{
+  // find the min and max value across all the input STEM images
+  // auto min = std::numeric_limits<float>::max();
+  // auto max = std::numeric_limits<float>::min();
+
+  // information about input STEMImage
+  int width = inImage.width;
+  int height = inImage.height;
+  auto curData = inImage.data;
+  std::cout << "Input STEM Image has width = " << width
+            << ", height = " << height << std::endl;
+
+  auto result = std::minmax_element(curData.get(), curData.get() + width * height);
+  uint64_t min = *result.first;
+  uint64_t max = *result.second;
+
+  // curImage.data is shared_ptr<uint64_t []>
+  // for (int i = 0; i < width * height; i++) {
+  //   // std::cout << "curData[" << i << "] = " << curData[i] << std::endl;
+  //   if (curData[i] < min)
+  //     min = curData[i];
+  //   if (curData[i] > max)
+  //     max = curData[i];
+  // }
+
+  // the "length" of each slot of the container
+  float length = static_cast<float>((max - min) / numBins);
+
+  std::vector<float> container;
+  // push the min value first
+  container.push_back(min);
+  // push all the intermediate values
+  for (int i = 1; i < numBins; i++) {
+    container.push_back(i * length);
+  }
+  // push the max value as the last number
+  container.push_back(max);
+
+  return container;
+}
+}
+
+
+// function that computes histogram for all the STEM images
+// each histogram is a vector<int>
+std::vector<int> createSTEMHistogram(STEMImage& inImage, const int numBins)
+{
+  // get the container of the histogram
+  std::vector<float> container = getContainer(inImage, numBins);
+
+  // initialize output
+  std::vector<std::vector<int>> allHistrograms;
+
+  // iterate through all the input STEM image
+  int width = inImage.width;
+  int height = inImage.height;
+  auto curData = inImage.data;
+  // for each image, get a histrogram
+  std::vector<int> histrogram(numBins, 0);
+  for (int i = 0; i < width * height; i++) {
+    auto value = curData[i];
+    // check which bin it belongs to
+    for (int i = 0; i < numBins; i++) {
+      if (value >= container[i] && value < container[i + 1]) {
+        histrogram[i] += 1;
+      }
+      // the max value is put in the last slot
+      else if (value == container[numBins]) {
+        histrogram[numBins - 1] += 1;
+      }
+    }
+  }
+
+  return histrogram;
+}
+
 vector<uint16_t> expandSparsifiedData(const vector<vector<uint32_t>>& data,
                                       size_t numPixels)
 {
@@ -590,85 +669,6 @@ RadialSum<uint64_t> radialSum(InputIt first, InputIt last, int scanWidth,
     future.get();
 
   return radialSum;
-}
-
-namespace {
-// helper function that returns the binding result
-std::vector<float> getContainer(const STEMImage& inImage, const int numBins)
-{
-  // find the min and max value across all the input STEM images
-  // auto min = std::numeric_limits<float>::max();
-  // auto max = std::numeric_limits<float>::min();
-
-  // information about input STEMImage
-  int width = inImage.width;
-  int height = inImage.height;
-  auto curData = inImage.data;
-  std::cout << "Input STEM Image has width = " << width
-            << ", height = " << height << std::endl;
-
-  auto result = std::minmax_element(curData.get(), curData.get() + width * height);
-  uint64_t min = *result.first;
-  uint64_t max = *result.second;
-
-  // curImage.data is shared_ptr<uint64_t []>
-  // for (int i = 0; i < width * height; i++) {
-  //   // std::cout << "curData[" << i << "] = " << curData[i] << std::endl;
-  //   if (curData[i] < min)
-  //     min = curData[i];
-  //   if (curData[i] > max)
-  //     max = curData[i];
-  // }
-
-  // the "length" of each slot of the container
-  float length = static_cast<float>((max - min) / numBins);
-
-  std::vector<float> container;
-  // push the min value first
-  container.push_back(min);
-  // push all the intermediate values
-  for (int i = 1; i < numBins; i++) {
-    container.push_back(i * length);
-  }
-  // push the max value as the last number
-  container.push_back(max);
-
-  return container;
-}
-}
-
-
-// function that computes histogram for all the STEM images
-// each histogram is a vector<int>
-std::vector<int> createSTEMHistogram(STEMImage& inImage, const int numBins)
-{
-  // get the container of the histogram
-  std::vector<float> container = getContainer(inImage, numBins);
-
-  // initialize output
-  std::vector<std::vector<int>> allHistrograms;
-
-  // iterate through all the input STEM image
-  int width = inImage.width;
-  int height = inImage.height;
-  auto curData = inImage.data;
-  // for each image, get a histrogram
-  std::vector<int> histrogram(numBins, 0);
-  for (int i = 0; i < width * height; i++) {
-    auto value = curData[i];
-    // check which bin it belongs to
-    for (int i = 0; i < numBins; i++) {
-      if (value >= container[i] && value < container[i + 1]) {
-        histrogram[i] += 1;
-      }
-      // the max value is put in the last slot
-      else if (value == container[numBins]) {
-        histrogram[numBins - 1] += 1;
-      }
-    }
-  }
-
-  return histrogram;
 }
 
 // Instantiate the ones that can be used
