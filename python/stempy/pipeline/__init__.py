300987ac557d4af6c4e887107866834180ec89b1
Alessandro Genova <alessandro.genova@kitware.com>
2019-07-04 13:46:33 -0400

    Add decorator to declare the arguments of a pipeline

--- python/stempy/pipeline/__init__.py
+++ python/stempy/pipeline/__init__.py
@@ -1,5 +1,5 @@
 import abc
-
+from functools import wraps
 
 class Pipeline(abc.ABC):
 
@@ -13,7 +13,26 @@ def pipeline(name=None, description=None):
     def _decorator(func):
         func.NAME = name
         func.DESCRIPTION = description
-
+        func.PARAMETERS = {}
         return func
+
+    return _decorator
+
+def parameter(name, type = 'string', label = None, description = None, default=None):
+
+    def _decorator(func):
+        func.PARAMETERS[name] = {
+            'type': type,
+            'label': label,
+            'description': description,
+            'default': default
+        }
+
+        @wraps(func)
+        def _wrapper(*args, **kwargs):
+            if name not in kwargs:
+                kwargs[name] = default
+            return func(*args, **kwargs)
+        return _wrapper
 
     return _decorator
