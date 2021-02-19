9a7b9ed88a2cf86f0c8920da95606bba4d3d81c2
Patrick Avery <patrick.avery@kitware.com>
2021-02-19 15:30:13 -0600

    Patch auditwheel to fix libstem.so rpath issue
    pypa/auditwheel#136 results in pure C++ libraries not getting patched
    by auditwheel. This is a problem for us, since our pure C++ library,
    libstem.so, needs its rpath patched for hdf5.
    
    pypa/auditwheel#283 fixes this issue, but is not yet merged. Until
    it gets merged and added to quay.io/pypa/manylinux2010_x86_64,
    we will patch auditwheel inside quay.io/pypa/manylinux2010_x86_64 to
    include this fix.
    
    Signed-off-by: Patrick Avery <patrick.avery@kitware.com>

--- scripts/github-actions/docker/build_and_push.sh
+++ scripts/github-actions/docker/build_and_push.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 image_base=quay.io/pypa/manylinux2010_x86_64
-tag=openchemistry/stempy_manylinux2010_x86_64
+tag=openchemistry/stempy_wheel_builder
 
 docker build . -t $tag --build-arg BASE_IMAGE=$image_base
 docker push $tag
