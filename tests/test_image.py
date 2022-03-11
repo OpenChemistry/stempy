1310582ffb6288603bf6361c4bc2f669f40ac0d2
Patrick Avery <patrick.avery@kitware.com>
2022-03-11 08:25:15 -0600

    Add com_sparse() test using real data
    This tests comparing the com_sparse results of real data with scipy's
    center_of_mass results for the dense version of the array.
    
    Signed-off-by: Patrick Avery <patrick.avery@kitware.com>

--- tests/test_image.py
+++ tests/test_image.py
@@ -1,10 +1,11 @@
 import numpy as np
+from scipy.ndimage import center_of_mass
 
 from stempy.image import com_sparse
 from stempy.io.sparse_array import SparseArray
 
 
-def test_com_sparse():
+def test_com_sparse(sparse_array_small, full_array_small):
     # Do a basic test of com_sparse() with multiple frames per scan
     # position.
     data = np.empty(4, dtype=object)
@@ -24,3 +25,16 @@ def test_com_sparse():
 
     assert np.array_equal(com[:, 0], [[0.5], [0.5]])
     assert np.array_equal(com[:, 1], [[0.25], [0.25]])
+
+    # Now, test and make sure the center of mass computed for some real
+    # data matches the dense array version.
+
+    com = com_sparse(sparse_array_small)
+
+    full_com = np.empty((2, *full_array_small.shape[:2]), dtype=np.float32)
+    for i in range(full_array_small.shape[0]):
+        for j in range(full_array_small.shape[1]):
+            # Need to swap com order, as com_sparse() does
+            full_com[:, i, j] = center_of_mass(full_array_small[i, j])[::-1]
+
+    assert np.array_equal(com, full_com)
