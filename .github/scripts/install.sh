7ac43a4c73b73d25774eb6b4d71f3765fa66f00b
Chris Harris <cjh@lbl.gov>
2024-09-13 13:22:34 +0000

    Add CIBW_BEFORE_ALL_MACOS to build HDF5
    The brew installed version has a too new min os version

--- .github/scripts/install.sh
+++ .github/scripts/install.sh
@@ -6,5 +6,5 @@ pip install cibuildwheel
 if [[ $RUNNER_OS == "Windows" ]]; then
     git clone --recursive -b 3.3.9 --depth 1 https://gitlab.com/libeigen/eigen /c/eigen
 elif [[ $RUNNER_OS == "macOS" ]]; then
-    brew install eigen hdf5 ninja
+    brew install eigen ninja
 fi
