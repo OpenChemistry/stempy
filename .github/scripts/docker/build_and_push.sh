18b3486cd75c45ce8039019b3b4ba15aa440d771
Sam Welborn <swelborn@lbl.gov>
2024-12-19 11:20:59 -0500

    aarch64 with qemu
    - eigen-devel not available on manylinux2014
    - was considering using 2_34 but we use older for potential compatibility issues
    - 2_28 based on alma linux, uses dnf
    - changed "latest" version of cmake. dnf installs 3.26.
    - try to avoid nested virtualization with multiplatform build

--- .github/scripts/docker/build_and_push.sh
+++ .github/scripts/docker/build_and_push.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
-image_base=quay.io/pypa/manylinux2014_x86_64
-tag=openchemistry/stempy_wheel_builder
+# arch=x86_64
+arch=aarch64
+image_base=quay.io/pypa/manylinux_2_28_${arch}
+tag=samwelborn/stempy_wheel_builder_${arch}
 
-docker build . -t $tag --build-arg BASE_IMAGE=$image_base
+docker build . --platform=linux/amd64,linux/arm64 -t $tag --build-arg BASE_IMAGE=$image_base --build-arg ARCH=$arch
 docker push $tag
