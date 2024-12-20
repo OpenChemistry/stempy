77531df7cd1b80526f86df22c138e0145bcd5686
Sam Welborn <swelborn@lbl.gov>
2024-12-20 13:22:56 -0500

    --platform matching and upgrade aarch builder image
    - attempt to match the platform properly in the builder image, don't build both when not needed

--- .github/scripts/docker/build_and_push.sh
+++ .github/scripts/docker/build_and_push.sh
@@ -2,8 +2,15 @@
 
 # arch=x86_64
 arch=aarch64
+
+if [ "$arch" = "x86_64" ]; then
+    platform=linux/amd64
+elif [ "$arch" = "aarch64" ]; then
+    platform=linux/arm64
+fi
+
 image_base=quay.io/pypa/manylinux_2_28_${arch}
 tag=samwelborn/stempy_wheel_builder_${arch}
 
-docker build . --platform=linux/amd64,linux/arm64 -t $tag --build-arg BASE_IMAGE=$image_base --build-arg ARCH=$arch
+docker build . --platform=$platform -t $tag --build-arg BASE_IMAGE=$image_base --build-arg ARCH=$arch
 docker push $tag
